f324385bcc7b6b60381fa5944123d02f09702d89
Rüdiger Klaehn <rklaehn@gmail.com>
2017-12-04 23:02:09 +0100

    Nice gaussian group aggregate

--- src/main/scala/aggregation/GaussianGroupAggregate.scala
+++ src/main/scala/aggregation/GaussianGroupAggregate.scala
@@ -1,17 +1,20 @@
 package aggregation
 
 import spire.algebra._
-import spire.syntax.field._
-import spire.syntax.nroot._
-import spire.syntax.eq._
+import spire.syntax.all._
 import spire.sp
 
 // https://izbicki.me/blog/gausian-distributions-are-monoids.html
+/**
+  * @param n the number of samples
+  * @param m the mean
+  * @param s the pre-weighted variance (variance * n)
+  */
 case class GaussianGroupAggregate[@sp T](n: Int, m: T, s: T) {
 
   def mean = m
 
-  def variance(implicit f: Field[T]): T = s / n
+  def variance(implicit g: Field[T]): T = s / n
 
   def sampleVariance(implicit f: Field[T]): T = s / (n - 1)
 
@@ -22,22 +25,18 @@ case class GaussianGroupAggregate[@sp T](n: Int, m: T, s: T) {
 
 object GaussianGroupAggregate {
 
-  def empty[@sp T: Field]: GaussianGroupAggregate[T] =
-    GaussianGroupAggregate(0, Field.zero[T], Field.zero[T])
+  def empty[@sp T: AdditiveMonoid]: GaussianGroupAggregate[T] =
+    GaussianGroupAggregate(0, AdditiveMonoid.zero[T], AdditiveMonoid.zero[T])
 
-  def apply[@sp T: Field](value: T): GaussianGroupAggregate[T] =
-    GaussianGroupAggregate(1, value, Field.zero[T])
+  def apply[@sp T: AdditiveMonoid](value: T): GaussianGroupAggregate[T] =
+    GaussianGroupAggregate(1, value, AdditiveMonoid.zero[T])
 
-  implicit def group[T: Field: Eq]: Monoid[GaussianGroupAggregate[T]] = new Monoid[GaussianGroupAggregate[T]] {
+  implicit def monoid[T: Field]: Monoid[GaussianGroupAggregate[T]] = new Monoid[GaussianGroupAggregate[T]] {
 
     override def empty = GaussianGroupAggregate.empty[T]
 
     override def combine(a: GaussianGroupAggregate[T], b: GaussianGroupAggregate[T]) =
-      if (a.n > b.n) combine0(a, b) else combine0(b, a)
-
-    // this assumes that a.n >= b.n
-    private def combine0(a: GaussianGroupAggregate[T], b: GaussianGroupAggregate[T]) =
-      if (b.n == 0) a else {
+      if (b.n == 0) a else if (a.n == 0) b else {
         val u = a.m
         val x = a.s
         val i = a.n
@@ -48,57 +47,20 @@ object GaussianGroupAggregate {
 
         val n: Int = i + j
 
-        // m = m0 + (x - m0) / n
-        // val m: T = u + (v - u) * j / n // this now degenerates to the welford method for b.n == 1
-
         // weighted average of means
         val m: T = (u * i + v * j) / n
 
-        // s = s0 + (x - m0) * (x - m)
-        // a.m == m0 == u
-        // a.s == s0 == x
-        // a.n       == i
-        // b.n == 1  == j
-        // b.s == 0  == y
-        // b.m == x  == v
-        // as + bs + an * am * am + bn * bm * bm - n * m * m
-        // x + y + i * u * u + j * v * v - n * m * m
-        // val s: T = x + y + i * u * u + j * v * v - (i + j) * (u + (v - u) * j / (i + j)) * (u + (v - u) * j / (i + j))
-
-        // val s: T = x +         y + i * u * u + j * v * v - (i + j) * (u + (v - u) * j / (i + j)) * (u + (v - u) * j / (i + j))
-
-        // val s: T = x + y + i * u * u + j * v * v - (i + j) * (u + (v - u) * j / (i + j)) * (u + (v - u) * j / (i + j))
-
-        // this one looks pretty good!
-        // val f = - (i * j * (u - v) * (u - v) + (j + i) * y) / ((2 * j + i) * (u - v))
-        // val s: T = x + f * (v - u + (v - u) * j / (i + j))
-
-        // val f = (y + j * (u * u - 2 * u * v + v * v - i * y)) / ((v - u) * (v - u))
-        // val vu = v - u
-        // val s0 = x + y + i * u * u + j * v * v - n * m * m
-
-        // weighted sum of moments, remember x and y are pre-weighted
+        // weighted sum of moments, remember x and y are pre-weighted. No floating point problems here.
         val s0 = x + y
 
         // for the moment caused by the mean being different, only the difference matters
         // it is crucial to calculate the result in terms of the difference to limit floating point errors
         val vu = v - u
 
-        //
+        // intuitively, this contribution is biggest if both are of the same weight ( i ~= j )
         val sd = (i * j * vu * vu) / n
-        val s: T = s0 + sd // x + y + i * u * u + j * v * v - (i + j) * (u + (v - u) * j / (i + j)) * (u + (v - u) * j / (i + j))
-//        val s0 = x + y + i * u * u + j * v * v - n * m * m
-//        val s = if(!vu.isZero) {
-//          val f = j + (1 - i * j) * y / (vu * vu)
-//          x + f * (v - u) * (v - m)
-//        } else {
-//          s0
-//        }
-//        if(!(s -s0).isZero) {
-//          println(s)
-//          println(s0)
-//        }
 
+        val s: T = s0 + sd
         GaussianGroupAggregate(n, m, s)
       }
   }
