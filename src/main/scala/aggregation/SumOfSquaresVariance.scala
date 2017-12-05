edc222660daae76866036ed9e812c8f6ff60a0e2
Rüdiger Klaehn <rklaehn@gmail.com>
2017-12-05 08:00:24 +0100

    Add stub for exercise 1b

---
+++ src/main/scala/aggregation/SumOfSquaresVariance.scala
@@ -0,0 +1,14 @@
+package aggregation
+
+import spire.syntax.all._
+import spire.implicits._
+
+object SumOfSquaresVariance {
+
+  /**
+    * Takes a sequence of values and computes count, mean and variance
+    */
+  def variance[T](values: Seq[T]): (Int, T, T) = {
+    ???
+  }
+}
